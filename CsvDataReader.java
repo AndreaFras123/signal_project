4fc7eb021927b58e3cda8ac05a4799a98ec511b3
AndreaFras123 <167615889+AndreaFras123@users.noreply.github.com>
2024-05-24 16:43:10 +0200

    Create CsvDataReader.java
    implemintation of interface

---
+++ CsvDataReader.java
@@ -0,0 +1,34 @@
+package com.data_management;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CsvDataReader implements DataReader {
+    private String filePath;
+
+    public CsvDataReader(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public void readData(DataStorage dataStorage) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Assuming CSV format: patientId,measurementValue,recordType,timestamp
+                String[] values = line.split(",");
+                int patientId = Integer.parseInt(values[0]);
+                double measurementValue = Double.parseDouble(values[1]);
+                String recordType = values[2];
+                long timestamp = Long.parseLong(values[3]);
+
+                // Add data to DataStorage
+                dataStorage.addPatientData(patientId, measurementValue, recordType, timestamp);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+}
